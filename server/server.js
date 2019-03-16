3b3d89f913da3e51873af34c1f762dfc9c3a502d
Sachin <sachinb443@gmail.com>
2019-03-16 21:36:53 +0530

    'post'

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization');
     if(req.method === 'OPTIONS')
     {
-        res.header('Access-Control-Allow-Methods','PUT','POST','PATCH','DELETE','GET');
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
     next();
@@ -47,7 +47,7 @@ app.get('/bankapp',(req,res)=>{
     })
 });
 
-app.get('/bankapp:id',(req,res)=>{
+app.get('/bankapp/:id',(req,res)=>{
     const id = req.params.id;
     if(!ObjectID.isValid(id)){
         return res.status(404).send();
