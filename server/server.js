0dbc207d4f6e56f6dd0e4c84a3579b3fd1baef11
Sachin <sachinb443@gmail.com>
2019-03-16 21:05:29 +0530

    cross origin

--- server/server.js
+++ server/server.js
@@ -8,6 +8,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 const port = process.env.PORT || 3000;
 
 app.post('/bankapp',(req,res)=>{
