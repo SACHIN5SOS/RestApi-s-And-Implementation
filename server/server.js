7e43e2ac0b1cc76c7a76948cf321c86a2ee41f9b
Sachin <sachinb443@gmail.com>
2019-03-17 13:09:02 +0530

    Rfactoring

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {BankApp} = require('./models/bankapp');
-const morgan  = require('morgan');
+
 
 const app = express();
 
@@ -13,9 +13,8 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use(function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     next();
 });
 
